d3e1a8b380836e795005282c9654c0c157c6190d
Alex <akosialexpo@gmail.com>
2017-04-19 01:21:04 +0800

    Initial

---
+++ app/app.py
@@ -0,0 +1,19 @@
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+def main():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
