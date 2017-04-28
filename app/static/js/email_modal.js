33ec64d7a4a52a8074713d09e6d98c29ac66c882
Alex <akosialexpo@gmail.com>
2017-04-29 04:04:39 +0800

    Add clipboard.js and finish email modal

--- app/static/js/email_modal.js
+++ app/static/js/email_modal.js
@@ -1,12 +1,22 @@
 var modal = document.getElementById("mail_modal");
 var btn = document.getElementById("modalBtn");
+var clipboard = new Clipboard(".copyEmail");
+
+clipboard.on("success", function(e) {
+  notify_success();
+});
+
+function notify_success() {
+  var modal_content = document.getElementsByClassName("modal_content")[0];
+  modal_content.style.background = "dodgerblue";
+  modal_content.style.background = "#1e1e20";
+}
 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
 window.onclick = function(event) {
-  console.log(event);
   if (event.target == modal) {
     modal.style.display = "none";
   }
